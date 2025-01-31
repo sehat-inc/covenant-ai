ce9b99f7a0baf30409a9c4a6b57307cc2db11092
vijdaancoding <alivijdaan@gmail.com>
2025-01-31 15:57:35 +0500

    file structure

---
+++ rag/main.py
@@ -0,0 +1,11 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv('GEMINI_API'))
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Who are you?")
+print(response.text)
